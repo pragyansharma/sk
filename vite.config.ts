ad95799c9e6a496fa9598b9765792a366e2292d9
pragyansharma <pragyansma@gmail.com>
2025-05-19 18:12:28 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
+// https://vitejs.dev/config/
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sk/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
